f80f25846a3ac514cbe4ad0af3fcc60160c347dd
dialex <dialex.nunes@gmail.com>
2016-01-01 19:16:25 +0000

    mock browsers

--- AdClicky/Form1.cs
+++ AdClicky/Form1.cs
@@ -12,8 +12,38 @@ using System.Windows.Forms;
 
 namespace AdClicky
 {
+    #region DomainModel classes
+
+    public class Browser
+    {
+        public string Name;
+        public string UserAgent;
+
+        public Browser(string name, string headerUserAgent)
+        {
+            Name = name;
+            UserAgent = headerUserAgent;
+        }
+
+        public static Browser CHROME    { get { return new Browser("Chrome", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"); } }
+        public static Browser SAFARI    { get { return new Browser("Safari", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"); } }
+        public static Browser FIREFOX   { get { return new Browser("Firefox", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"); } }
+        public static Browser IEXPLORER { get { return new Browser("IE", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"); } }
+        public static Browser SEAMONKEY { get { return new Browser("SeaMonkey", "Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre"); } }
+        public static Browser KONQUEROR { get { return new Browser("Konqueror", "Mozilla/5.0 (X11; Linux) KHTML/4.9.1 (like Gecko) Konqueror/4.9"); } }
+        public static Browser OPERA     { get { return new Browser("Opera", "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16"); } }
+    }
+
+    #endregion
+
     public partial class Form1 : Form
     {
+        #region Data
+
+        public static Browser[] BrowserChoices = { Browser.CHROME, Browser.SAFARI, Browser.FIREFOX, Browser.IEXPLORER };
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
@@ -58,7 +88,8 @@ namespace AdClicky
 
         void SimulateBrowsing(string url)
         {
-            MockBrowser();
+            //Browser browserHeader = GetRandomBrowser();
+            Browser browserHeader = Browser.OPERA;
             MockCountry();
             OpenUrl(url);
             MockReadingTime(10);
@@ -80,18 +111,27 @@ namespace AdClicky
             throw new NotImplementedException();
         }
 
-        private void MockBrowser()
+        private Browser GetRandomBrowser()
         {
-            throw new NotImplementedException();
+            int randIndex = new Random().Next(0, BrowserChoices.Length - 1);
+            return BrowserChoices[randIndex];
         }
 
         #endregion
         #region URL
 
-        private void OpenUrl(string url)
+        private void OpenUrl(string url, Browser mockBrowser = null)
         {
-            Log("Open on fake browser: " + url);
-            webBrowser1.Navigate(url);
+            Log("Open on browser: " + url);
+            if (mockBrowser == null)
+            {
+                webBrowser1.Navigate(url);
+            }
+            else
+            {
+                Log("\UserAgent: " + mockBrowser.Name);
+                webBrowser1.Navigate(url, "_self", null, mockBrowser.UserAgent + Environment.NewLine);
+            }
 
             Log("Downloading...", false);
             while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
@@ -101,12 +141,15 @@ namespace AdClicky
             Log(" done", true);
         }
 
-        private string FetchUrl(string url)
+        private string FetchUrl(string url, string hearderUserAgent = "")
         {
             Log("Fetch via code: " + url);
-            string htmlPage = new WebClient().DownloadString(url);
+            WebClient web = new WebClient();
 
-            return htmlPage;
+            if (hearderUserAgent != string.Empty)
+                web.Headers.Add("user-agent", hearderUserAgent);
+
+            return web.DownloadString(url);
         }
 
         #endregion
